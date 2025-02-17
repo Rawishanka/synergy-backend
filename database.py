abe798b0ec4c332f38e3b39ad2ccc6446679458e
rasinduRawishanka <63003929+Rawishanka@users.noreply.github.com>
2025-02-17 17:48:25 +0530

    update database url

--- database.py
+++ database.py
@@ -3,9 +3,9 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 
-DATABASE_URL = "postgresql+asyncpg://postgres:Rasindu@localhost:5432/testdb"
+#DATABASE_URL = "postgresql+asyncpg://postgres:Rasindu@localhost:5432/testdb"
 #DATABASE_URL = "postgresql://adscraperx_owner:1mYZWdiKO6tJ@ep-ancient-union-a5ivb14v.us-east-2.aws.neon.tech/adscraperx?sslmode=require"
-#DATABASE_URL = "postgresql+asyncpg://adscraperx_owner:1mYZWdiKO6tJ@ep-ancient-union-a5ivb14v.us-east-2.aws.neon.tech/adscraperx"
+DATABASE_URL = "postgresql+asyncpg://adscraperx_owner:1mYZWdiKO6tJ@ep-ancient-union-a5ivb14v.us-east-2.aws.neon.tech/adscraperx"
 
 # Create the database engine
 engine = create_async_engine(DATABASE_URL)
